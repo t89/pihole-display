b5ae7b50ae5f59956f14afa529fd356f1f79b611
Thomas Johannesmeyer <thomas@geeky.gent>
2020-06-30 19:42:02 -0500

    Add setup

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Fixes backspace issues when using ssh
+export TERM=linux
+
+# If these two dont work, use sudo raspi-config > Interface Options and
+# activate SPI / I2S manually
+
+# Enable SPI
+sudo sed -i 's/dtparam=spi=off/dtparam=spi=on/g' /boot/config.txt
+
+# Enable i2s
+sudo sed -i 's/dtparam=i2s_arm=off/dtparam=o2s_arm=on/g' /boot/config.txt
+
+# System upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt update -y
+# full-upgrade is used in preference to a simple upgrade, as it also picks up any dependency changes that may have been made
+sudo apt full-upgrade -y
+
+sudo apt install python3-pip -y
+
+# LED Display
+sudo apt-get install libtiff5 -y
+sudo apt-get install libopenjp2-7 -y
+sudo pip3 install --upgrade adafruit-circuitpython-ssd1306
+
+#RGB Display
+sudo pip3 install --upgrade adafruit circuitpython-rgb-display
+
+# Both
+sudo pip3 install --upgrade adafruit-blinka spidev pillow linux_metrics
+##
