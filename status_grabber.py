fd8efd445e3547412aa6819db7afe17fedf2ffa9
Thomas Johannesmeyer <thomas@geeky.gent>
2020-06-30 20:21:25 -0500

    Fix missing arrows

--- status_grabber.py
+++ status_grabber.py
@@ -117,4 +117,45 @@ class StatusGrabber():
         if (weather['probability'] is ''):
             weather['probability'] = '0'
 
+        ##
+        # Fix missing arrow chars in font PressStart2P
+        default_leftwards_arrow = b'\xE2\x86\x90'.decode()
+        default_upwards_arrow = b'\xE2\x86\x91'.decode()
+        default_downwards_arrow = b'\xE2\x86\x93'.decode()
+        default_rightwards_arrow = b'\xE2\x86\x92'.decode()
+        default_northwest_arrow = b'\xE2\x86\x96'.decode()
+        default_northeast_arrow = b'\xE2\x86\x97'.decode()
+        default_southwest_arrow = b'\xE2\x86\x99'.decode()
+        default_southeast_arrow = b'\xE2\x86\x98'.decode()
+
+        fixed_leftwards_arrow = b'\xE2\x86\x90'.decode()
+        fixed_upwards_arrow = b'\xE2\x86\x91'.decode()
+        fixed_downwards_arrow = b'\xE2\x86\x93'.decode()
+        fixed_rightwards_arrow = b'\xE2\x86\x92'.decode()
+        fixed_northwest_arrow = b'\xE2\x86\x91\xE2\x86\x90'.decode()
+        fixed_northeast_arrow = b'\xE2\x86\x91\xE2\x86\x92'.decode()
+        fixed_southwest_arrow = b'\xE2\x86\x93\xE2\x86\x90'.decode()
+        fixed_southeast_arrow = b'\xE2\x86\x93\xE2\x86\x92'.decode()
+
+        default_arrows = [default_leftwards_arrow,
+                           default_upwards_arrow,
+                           default_downwards_arrow,
+                           default_rightwards_arrow,
+                           default_northwest_arrow,
+                           default_northeast_arrow,
+                           default_southwest_arrow,
+                           default_southeast_arrow]
+
+        fixed_arrows = [fixed_leftwards_arrow,
+                           fixed_upwards_arrow,
+                           fixed_downwards_arrow,
+                           fixed_rightwards_arrow,
+                           fixed_northwest_arrow,
+                           fixed_northeast_arrow,
+                           fixed_southwest_arrow,
+                           fixed_southeast_arrow]
+
+        for idx, _ in enumerate(default_arrows):
+            weather['wind'] = weather['wind'].replace(default_arrows[idx], fixed_arrows[idx])
+
         self.weather = weather
