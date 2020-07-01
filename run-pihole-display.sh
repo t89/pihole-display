629dc3dba2c6a6dde673b7008a95aa35b1113d75
Thomas Johannesmeyer <thomas@geeky.gent>
2020-07-01 10:47:08 -0500

    Fix auto-run-script

--- run-pihole-display.sh
+++ run-pihole-display.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
 git pull origin master
 
-python3 ./led_display.py &
+python3 "$DIR/led_display.py &"
