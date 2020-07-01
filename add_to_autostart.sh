cea9ddde498663456cd0ada5613ec552513c8c37
Thomas Johannesmeyer <thomas@geeky.gent>
2020-07-01 10:39:38 -0500

    Add autostart script

---
+++ add_to_autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# See https://stackoverflow.com/a/246128/3561275
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+script_name="run-pihole-display.sh"
+auto_start_entry_available="$(grep '$script_name' /etc/rc.local | wc -l)"
+if [! "$auto_start_entry_available" -eq "1" ]; then
+    echo "$script_name already in /etc/rc.local"
+else
+    # add to auto start
+    sudo echo "$DIR/$script_name &" >> /etc/rc.local
+fi
