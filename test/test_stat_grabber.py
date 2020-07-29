db9192acda8c3d86b31f697518aa0384a7b0959c
Thomas Johannesmeyer <thomas@geeky.gent>
2020-07-28 22:04:19 -0500

    Remove old comments

--- test/test_stat_grabber.py
+++ test/test_stat_grabber.py
@@ -25,33 +25,3 @@ def test_active_network_device_count():
 
     # Return value is integer
     assert isinstance(result, int)
-
-# import unittest
-
-# from src.stat_grabber import StatGrabber as StatGrabberClass
-
-
-# class Test(unittest.TestCase):
-#     """
-#     The basic class that inherits unittest.TestCase
-#     """
-
-#     stat_grabber = StatGrabberClass()
-
-#     def test_get_local_ip(self):
-#         """ Check for return value, evaluate IP format """
-
-#         print("Start set_name test\n")
-
-#         ip = self.stat_grabber.get_local_ip()
-#         self.assertIsNotNone(ip)
-
-#         desired_result = 4
-#         result = len(ip.split('.'))
-
-#         # Ip has format x.x.x.x
-#         self.assertEqual(result, desired_result)
-
-# if __name__ == '__main__':
-#     # begin the unittest.main()
-#     unittest.main()
