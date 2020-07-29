3f4343bc67155b0b1caebf733c618991cf4d32a9
Thomas Johannesmeyer <thomas@geeky.gent>
2020-07-28 21:24:53 -0500

    Add initial test

---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+# conftest.py
+
+import pytest
+import requests
+
+@pytest.fixture(autouse=False)
+def disable_network_calls(monkeypatch):
+    def stunted_get():
+        raise RuntimeError("Network access not allowed during testing!")
+    monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
---
+++ test/test_stat_grabber.py
@@ -0,0 +1,48 @@
+import pytest
+from src.stat_grabber import StatGrabber
+
+stat_grabber = StatGrabber()
+
+@pytest.mark.requires_network
+@pytest.mark.linux
+def test_get_local_ip():
+    ip = stat_grabber.get_local_ip()
+    print(ip)
+    assert ip != None
+
+    desired_result = 4
+    result = len(ip.split('.'))
+
+    # Ip has format x.x.x.x
+    assert result == desired_result
+
+
+# import unittest
+
+# from src.stat_grabber import StatGrabber as StatGrabberClass
+
+
+# class Test(unittest.TestCase):
+#     """
+#     The basic class that inherits unittest.TestCase
+#     """
+
+#     stat_grabber = StatGrabberClass()
+
+#     def test_get_local_ip(self):
+#         """ Check for return value, evaluate IP format """
+
+#         print("Start set_name test\n")
+
+#         ip = self.stat_grabber.get_local_ip()
+#         self.assertIsNotNone(ip)
+
+#         desired_result = 4
+#         result = len(ip.split('.'))
+
+#         # Ip has format x.x.x.x
+#         self.assertEqual(result, desired_result)
+
+# if __name__ == '__main__':
+#     # begin the unittest.main()
+#     unittest.main()
