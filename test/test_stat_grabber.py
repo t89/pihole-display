fd7be689d1857dc2cfd3a159a90a60f6eaac0578
Thomas Johannesmeyer <thomas@geeky.gent>
2020-07-28 22:06:12 -0500

    Order marks alphabetically

--- test/test_stat_grabber.py
+++ test/test_stat_grabber.py
@@ -3,8 +3,8 @@ from src.stat_grabber import StatGrabber
 
 stat_grabber = StatGrabber()
 
-@pytest.mark.requires_network
 @pytest.mark.linux
+@pytest.mark.requires_network
 def test_get_local_ip():
     ip = stat_grabber.get_local_ip()
 
@@ -16,10 +16,10 @@ def test_get_local_ip():
     # Ip has format x.x.x.x
     assert result == desired_result
 
-@pytest.mark.sudo
-@pytest.mark.slow
-@pytest.mark.mac
 @pytest.mark.linux
+@pytest.mark.mac
+@pytest.mark.slow
+@pytest.mark.sudo
 def test_active_network_device_count():
     result = stat_grabber.get_active_network_device_count()
     assert result != None
