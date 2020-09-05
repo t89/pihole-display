13f83e241a16ff7a37f0c063a8bf38e9cc426b8c
Thomas Johannesmeyer <thomas@geeky.gent>
2020-09-05 18:51:58 -0500

    Fix paths

--- version/admin-update.sh
+++ version/admin-update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 echo "Checking Web / Admin"
 
-read -r destination_version < admin-version
+read -r destination_version < "$DIR/admin-version"
 current_version="$(pihole -v -a | awk '{print $4;}')"
 
 if [[ $current_version == *"release/"* ]]; then
