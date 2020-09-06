0824f509a5c7478795f0ffdf5f4f7897b9e1ac17
Thomas Johannesmeyer <thomas@geeky.gent>
2020-09-05 19:07:37 -0500

    Adjust order of controlled update subscripts

--- controlled_update.sh
+++ controlled_update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-bash "./version/ftl-update.sh"
-bash "./version/admin-update.sh"
+##
+# I _think_ core has to be first, because it seems to automatically
+# update everything else to the _newest_ version. Therefore we have to
+# enforce the other versions afterwards
 bash "./version/core-update.sh"
+bash "./version/admin-update.sh"
+bash "./version/ftl-update.sh"
