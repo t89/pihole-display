f62354fdf17f890c4cafa7dc572c0fd8495ec35b
Thomas Johannesmeyer <thomas@geeky.gent>
2020-09-13 14:36:02 -0500

    Adjust ip

--- dev_update.sh
+++ dev_update.sh
@@ -11,13 +11,13 @@ rm known_clients
 rm dev_update.sh
 rm anim/intro.gif
 
-wget 192.168.100.247:8000/src/stat_grabber.py -P ./src/
-wget 192.168.100.247:8000/src/led_display.py -P ./src/
-wget 192.168.100.247:8000/src/__main__.py -P ./src/
-wget 192.168.100.247:8000/src/observer.py -P ./src/
-wget 192.168.100.247:8000/src/housekeeper.py -P ./src/
-wget 192.168.100.247:8000/src/network.py -P ./src/
-wget 192.168.100.247:8000/location
-wget 192.168.100.247:8000/known_clients
-wget 192.168.100.247:8000/dev_update.sh
-wget 192.168.100.247:8000/anim/intro.gif -P ./anim/
+wget 192.168.1.212:8000/src/stat_grabber.py -P ./src/
+wget 192.168.1.212:8000/src/led_display.py -P ./src/
+wget 192.168.1.212:8000/src/__main__.py -P ./src/
+wget 192.168.1.212:8000/src/observer.py -P ./src/
+wget 192.168.1.212:8000/src/housekeeper.py -P ./src/
+wget 192.168.1.212:8000/src/network.py -P ./src/
+wget 192.168.1.212:8000/location
+wget 192.168.1.212:8000/known_clients
+wget 192.168.1.212:8000/dev_update.sh
+wget 192.168.1.212:8000/anim/intro.gif -P ./anim/
